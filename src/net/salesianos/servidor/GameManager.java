e7ec2076a9f47665e670862ff08f3e3ade604c6d
hortuadev <hortuadev@gmail.com>
2026-04-25 21:53:28 +0100

    feat(server): create GameManager class with game constants

---
+++ src/net/salesianos/servidor/GameManager.java
@@ -0,0 +1,9 @@
+package net.salesianos.servidor;
+
+public class GameManager {
+
+    public static final int MIN_JUGADORES = 2;
+    public static final int MAX_JUGADORES = 4;
+    public static final int TOTAL_RONDAS = 5;
+
+}
