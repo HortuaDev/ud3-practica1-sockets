f9d882609a0d0630a89152cac31422e0de9825ba
hortuadev <hortuadev@gmail.com>
2026-04-25 22:05:56 +0100

    feat(server): add main method with ServerSocket loop to ServidorDados

--- src/net/salesianos/servidor/ServidorDados.java
+++ src/net/salesianos/servidor/ServidorDados.java
@@ -1,7 +1,33 @@
 package net.salesianos.servidor;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServidorDados {
 
     private static final int PUERTO = 5000;
 
+    public static void main(String[] args) {
+
+        GameManager gameManager = new GameManager();
+
+        try (ServerSocket serverSocket = new ServerSocket(PUERTO)) {
+
+            System.out.println("Servidor iniciado en puerto " + PUERTO);
+
+            while (true) {
+
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Nueva conexión desde: "
+                        + clientSocket.getInetAddress().getHostAddress());
+
+                ClientHandler handler = new ClientHandler(clientSocket, gameManager);
+                handler.start();
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error al iniciar servidor: " + e.getMessage());
+        }
+    }
 }
