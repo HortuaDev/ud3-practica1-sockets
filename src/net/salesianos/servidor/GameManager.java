0b0bdc81d3f46c077fd97106e93ec8209b000d02
hortuadev <hortuadev@gmail.com>
2026-04-25 21:54:11 +0100

    feat(server): add GameManager state properties

--- src/net/salesianos/servidor/GameManager.java
+++ src/net/salesianos/servidor/GameManager.java
@@ -1,9 +1,21 @@
 package net.salesianos.servidor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class GameManager {
 
     public static final int MIN_JUGADORES = 2;
     public static final int MAX_JUGADORES = 4;
     public static final int TOTAL_RONDAS = 5;
+
+    private final List<Jugador> jugadores = new ArrayList<>();
+
+    private int rondaActual = 0;
+    private int turnoActual = 0;
+    private boolean partidaEnCurso = false;
+
+    private final Random random = new Random();
 
 }
