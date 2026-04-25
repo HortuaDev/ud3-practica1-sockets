41b63973caa3810bedf22ee849bd8de63809d7c1
hortuadev <hortuadev@gmail.com>
2026-04-25 21:45:39 +0100

    feat(server): add Jugador properties and stream fields

--- src/net/salesianos/servidor/Jugador.java
+++ src/net/salesianos/servidor/Jugador.java
@@ -1,5 +1,18 @@
 package servidor;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.io.IOException;
+
 public class Jugador {
+
+    private String nombre;
+    private int puntuacion;
+
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
 
 }
