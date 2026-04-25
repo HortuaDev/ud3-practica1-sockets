28f8caa923a7af44495c4964f8bd86d889cf417b
hortuadev <hortuadev@gmail.com>
2026-04-25 21:56:28 +0100

    feat(server): add synchronized player management methods to GameManager

--- src/net/salesianos/servidor/GameManager.java
+++ src/net/salesianos/servidor/GameManager.java
@@ -11,11 +11,36 @@ public class GameManager {
     public static final int TOTAL_RONDAS = 5;
 
     private final List<Jugador> jugadores = new ArrayList<>();
-
     private int rondaActual = 0;
     private int turnoActual = 0;
     private boolean partidaEnCurso = false;
-
     private final Random random = new Random();
+
+    public synchronized boolean agregarJugador(Jugador jugador) {
+        if (jugadores.size() >= MAX_JUGADORES || partidaEnCurso)
+            return false;
+        jugadores.add(jugador);
+        return true;
+    }
+
+    public synchronized void eliminarJugador(Jugador jugador) {
+        jugadores.remove(jugador);
+        broadcast("DESCONECTADO:" + jugador.getNombre());
+        if (turnoActual >= jugadores.size() && !jugadores.isEmpty()) {
+            turnoActual = 0;
+        }
+    }
+
+    public synchronized boolean haySuficientesJugadores() {
+        return jugadores.size() >= MIN_JUGADORES;
+    }
+
+    public synchronized boolean isPartidaEnCurso() {
+        return partidaEnCurso;
+    }
+
+    public synchronized int getNumJugadores() {
+        return jugadores.size();
+    }
 
 }
